fcf0bdb2026ac7429cf088afa0f4624d90fb3914
HomeDweller <asm886ith@gmail.com>
2022-08-31 21:28:08 +0200

    first commit

---
+++ script/script.js
@@ -0,0 +1,35 @@
+//task 1
+/*
+let name = prompt("Як вас звуть?");
+alert("Привіт " + name);
+*/
+//task 2
+/*
+let currentYear = 2022;
+let userYear = +prompt("Введіть рік вашого народження");
+alert("Вам " + currentYear - userYear +" років");
+*/
+//task 3
+/*
+let width = +prompt("Введіть довжину сторони квадрата в сантиметрах");
+alert("Периметер квадрата дорівнює " + width * 4 + " см");
+*/
+//task 4
+/*
+let radius = +prompt("Введіть довжину радіуса кола, в сантиметрах");
+let area = radius ** 2 * 3.14;
+alert("Площа кола дорівнює " + area + "см*2");
+*/
+//task 5
+/*
+let distance = +prompt("Введіть відстань у км");
+let hours = +prompt("Введіть час у годинах");
+let speed = distance / hours;
+alert("Швидкість дорівнює "+ speed + " км/год");
+*/
+//task 6
+/*
+let dollars = +prompt("Введіть певну кількість доларів");
+let ratio = 1;
+alert("Результат: "+ dollars * ratio +" євро");
+*/
